d9bc62d1294ae2ae6c7cadc62ba1af6525519203
Nolan Tran <72712954+ArvalCode@users.noreply.github.com>
2024-03-28 11:29:18 -0500

    3/28/24
    Mid class save

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    dir_list = os.listdir("music") 
+    dir_list = os.listdir("static/music") 
     return render_template('home.html', the_title='Music Player App', music = dir_list)
 
 @app.route('/createfolder', methods=['POST'])
@@ -14,7 +14,7 @@ def createfolder():
     name = request.form['collection_name']
     cover_image = request.files['cover_image']
     audio_files = request.files.getlist('files[]')
-    collection_folder = os.path.join('music', name) 
+    collection_folder = os.path.join('static/music', name) 
     os.makedirs(collection_folder, exist_ok=True) 
 
     cover_image_path = os.path.join(collection_folder, cover_image.filename) 
@@ -26,9 +26,24 @@ def createfolder():
     for audio_file in audio_files: 
         audio_file_path = os.path.join(audio_folder, audio_file.filename) 
         audio_file.save(audio_file_path)
-    dir_list = os.listdir("music") 
+    dir_list = os.listdir("static/music") 
 
     return redirect('/')
+
+@app.route('/folder')
+def folder():
+    folderid = request.args.get("foldername")
+    folder = os.listdir(f"static/music/{folderid}") 
+
+    audiofiles = os.listdir(f"static/music/{folderid}/audio")
+
+    for path in os.scandir(f'static/music/{folderid}'):
+        if path.is_file():
+            coverimage = path
+
+    
+    return render_template('yourlib.html', the_title='Your Library', audiofiles = audiofiles, name = folderid, coverimg = coverimage.name)
+
 
 if __name__ == '__main__':
     app.run(debug=True)    
